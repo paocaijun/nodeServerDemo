5b0e500719b87b63113128314e65114e5ca4da60
zr_dengxue <1097133606@qq.com>
2019-03-29 17:24:13 +0800

    update

--- level1/server.js
+++ level1/server.js
@@ -2,17 +2,33 @@ var http = require("http")
 var fs = require("fs")
 var url = require("url")
 var path = require("path")
+var DIR = "demo1"
+let types = {
+	html: "text/html",
+	ico: "image/x-icon",
+	jpeg: "image/jpeg",
+	jpg: "image/jpeg"
+}
 var server = http.createServer(function(request, response) {
-	// var pathname = url.parse(request.url).pathname
+	var pathname = url.parse(request.url).pathname
+	var filepath = path.join(DIR, pathname)
+	var ext = path.extname(filepath)
+	ext = ext ? ext.slice(1) : "unknown"
+	console.log("111", request.url, ext)
+
 	// console.log("pathname", pathname, url.parse(request.url))
 	// 第一个参数为相对路径
-	fs.readFile("index.html", "binary", function(err, file) {
+	if (ext == "ico") {
+		response.end()
+		return
+	}
+	fs.readFile(filepath, "binary", function(err, file) {
 		if (err) {
 			response.write(err)
 			response.end()
 		} else {
 			response.writeHead(200, {
-				"Content-Type": "text/html"
+				"Content-Type": types[ext]
 			})
 			response.write(file, "binary")
 			response.end()
